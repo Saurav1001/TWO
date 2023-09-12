666569280eb73fc9df3e4b43b662b531b574596b
Saurav1001 <sauravtyagi1212@gmail.com>
2023-09-12 11:03:37 +0530

    commenting Daily File

--- src/main/java/WebDriver/DailyJava.java
+++ src/main/java/WebDriver/DailyJava.java
@@ -11,23 +11,23 @@ public class DailyJava {
 	static WebDriver driver;  
 	public static void main(String[] args) {
 		
-		driver=new ChromeDriver();
-		driver.get("https://www.w3schools.com/html/html_tables.asp");
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
-		String value="Germany";
-		int rowCount=driver.findElements(By.xpath("//table[@id='customers']/tbody/tr")).size();
-		int colCount=driver.findElements(By.xpath("//table[@id='customers']/tbody/tr/th")).size();
-		boolean flag=false;
-		for(int i=1;i<rowCount;i++) {
-			for(int j=1;j<=colCount;j++) {
-			
-				String text=driver.findElement(By.xpath("//table[@id='customers']/tbody/tr["+(i+1)+"]/td["+j+"]")).getText();
-				System.out.println(i + " "+ j);
-				if(text.equals(value)) {
-					flag= true;
-				}
-			}
-		}System.out.println(flag);
+//		driver=new ChromeDriver();
+//		driver.get("https://www.w3schools.com/html/html_tables.asp");
+//		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+//		String value="Germany";
+//		int rowCount=driver.findElements(By.xpath("//table[@id='customers']/tbody/tr")).size();
+//		int colCount=driver.findElements(By.xpath("//table[@id='customers']/tbody/tr/th")).size();
+//		boolean flag=false;
+//		for(int i=1;i<rowCount;i++) {
+//			for(int j=1;j<=colCount;j++) {
+//			
+//				String text=driver.findElement(By.xpath("//table[@id='customers']/tbody/tr["+(i+1)+"]/td["+j+"]")).getText();
+//				System.out.println(i + " "+ j);
+//				if(text.equals(value)) {
+//					flag= true;
+//				}
+//			}
+//		}System.out.println(flag);
 		}
 		
 	}
